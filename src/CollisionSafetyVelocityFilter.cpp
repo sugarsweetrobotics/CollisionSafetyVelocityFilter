3dab25bfd6b2d0001e3d927050c9ff6135c82a08
Yuki Suga <ysuga@ysuga.net>
2017-02-19 22:48:52 +0900

    First Commit

---
+++ src/CollisionSafetyVelocityFilter.cpp
@@ -0,0 +1,210 @@
+// -*- C++ -*-
+/*!
+ * @file  CollisionSafetyVelocityFilter.cpp
+ * @brief Collision Avoider using Range Sensor for Mobile Robots
+ * @date $Date$
+ *
+ * $Id$
+ */
+
+#include "CollisionSafetyVelocityFilter.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* collisionsafetyvelocityfilter_spec[] =
+  {
+    "implementation_id", "CollisionSafetyVelocityFilter",
+    "type_name",         "CollisionSafetyVelocityFilter",
+    "description",       "Collision Avoider using Range Sensor for Mobile Robots",
+    "version",           "1.0.0",
+    "vendor",            "SugarSweetRobotics",
+    "category",          "Navigatio",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    // Configuration variables
+    "conf.default.deadRangeAngleRadian", "0.78",
+    "conf.default.maxRangeAngleRadian", "1.57",
+    "conf.default.deadDistanceMeter", "0.1",
+    "conf.default.maxDistanceMeter", "1.0",
+    "conf.default.distanceToForce", "1.0",
+    "conf.default.FxToVx", "0.01",
+    "conf.default.FxToVy", "0.0",
+    "conf.default.FxToVa", "0.0",
+    "conf.default.FyToVx", "0.0",
+    "conf.default.FyToVy", "0.1",
+    "conf.default.FyToVa", "0.1",
+    "conf.default.debug", "0",
+    // Widget
+    "conf.__widget__.deadRangeAngleRadian", "text",
+    "conf.__widget__.maxRangeAngleRadian", "text",
+    "conf.__widget__.deadDistanceMeter", "text",
+    "conf.__widget__.maxDistanceMeter", "text",
+    "conf.__widget__.distanceToForce", "text",
+    "conf.__widget__.FxToVx", "text",
+    "conf.__widget__.FxToVy", "text",
+    "conf.__widget__.FxToVa", "text",
+    "conf.__widget__.FyToVx", "text",
+    "conf.__widget__.FyToVy", "text",
+    "conf.__widget__.FyToVa", "text",
+    "conf.__widget__.debug", "text",
+    // Constraints
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+CollisionSafetyVelocityFilter::CollisionSafetyVelocityFilter(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_rangeIn("range", m_range),
+    m_VinIn("Vin", m_Vin),
+    m_VoutOut("Vout", m_Vout)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+CollisionSafetyVelocityFilter::~CollisionSafetyVelocityFilter()
+{
+}
+
+
+
+RTC::ReturnCode_t CollisionSafetyVelocityFilter::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("range", m_rangeIn);
+  addInPort("Vin", m_VinIn);
+  
+  // Set OutPort buffer
+  addOutPort("Vout", m_VoutOut);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // Bind variables and configuration variable
+  bindParameter("deadRangeAngleRadian", m_deadRangeAngleRadian, "0.78");
+  bindParameter("maxRangeAngleRadian", m_maxRangeAngleRadian, "1.57");
+  bindParameter("deadDistanceMeter", m_deadDistanceMeter, "0.1");
+  bindParameter("maxDistanceMeter", m_maxDistanceMeter, "1.0");
+  bindParameter("distanceToForce", m_distanceToForce, "1.0");
+  bindParameter("FxToVx", m_FxToVx, "0.01");
+  bindParameter("FxToVy", m_FxToVy, "0.0");
+  bindParameter("FxToVa", m_FxToVa, "0.0");
+  bindParameter("FyToVx", m_FyToVx, "0.0");
+  bindParameter("FyToVy", m_FyToVy, "0.1");
+  bindParameter("FyToVa", m_FyToVa, "0.1");
+  bindParameter("debug", m_debug, "0");
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t CollisionSafetyVelocityFilter::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t CollisionSafetyVelocityFilter::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t CollisionSafetyVelocityFilter::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+RTC::ReturnCode_t CollisionSafetyVelocityFilter::onActivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t CollisionSafetyVelocityFilter::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t CollisionSafetyVelocityFilter::onExecute(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t CollisionSafetyVelocityFilter::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t CollisionSafetyVelocityFilter::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t CollisionSafetyVelocityFilter::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t CollisionSafetyVelocityFilter::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t CollisionSafetyVelocityFilter::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+ 
+  void CollisionSafetyVelocityFilterInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(collisionsafetyvelocityfilter_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<CollisionSafetyVelocityFilter>,
+                             RTC::Delete<CollisionSafetyVelocityFilter>);
+  }
+  
+};
+
+
